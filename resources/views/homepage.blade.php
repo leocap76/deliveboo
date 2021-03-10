61978dd4b31f708e46df1d90f185aeea188521b9
Leonardo Capogna <leonardocapogna@gmail.com>
2021-03-10 12:07:33 +0100

    partials public

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <title>Deliveboo</title> 
+    </head>
+
+    
+    <body>
+        <!-- header -->
+        <header>
+
+            <h1>
+
+            </h1>
+
+        </header>
+    
+    </body>
+</html>

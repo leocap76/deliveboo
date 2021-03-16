a264ff1c5f1e33875ab0605f7401d262d85ab6a6
Leonardo Capogna <leonardocapogna@gmail.com>
2021-03-17 00:04:08 +0100

    img categories edited

--- config/categories.php
+++ config/categories.php
@@ -4,19 +4,19 @@
             "name" => "Pizza",
             "color" => "green",
             "description" => "La pizza è un prodotto gastronomico salato che consiste in un impasto a base di farina, acqua e lievito che viene spianato e condito tipicamente con pomodoro, mozzarella e altri ingredienti e cotto in un forno a legna",
-            "img_path" => "https://icons.iconarchive.com/icons/graphicloads/food-drink/256/pizza-icon.png"
+            "img_path" => "pizza-icon-fast-food-collection-food-icon-isolated_138676-504.jpg"
         ],
         [
             "name" => "Giapponese",
             "color" => "purple",
             "description" => "Cibi provienti dalla tradazione giapponese",
-            "img_path" => "https://image.flaticon.com/icons/png/512/174/174295.png"
+            "img_path" => "giapponese.jpg"
         ],
         [
             "name" => "Dolci",
             "color" => "pink",
             "description" => "Pasticcieria artigianale, solo vere tradizioni",
-            "img_path" => "https://www.clipartmax.com/png/full/41-415085_birthday-cake-free-icon-birthday-flat-icon-png.png"
+            "img_path" => "dolci.jpg"
         ],
         [
             "name" => "Indiano",
@@ -28,25 +28,25 @@
             "name" => "Messicano",
             "color" => "yellow",
             "description" => "La cucina messicana è caratterizzata da sapori intensi e vari, per via della gran varietà di spezie utilizzate. La gastronomia messicana, se si guarda ai sapori e agli ingredienti, è una delle più ricche del mondo in proteine, vitamine e minerali.",
-            "img_path" => "https://img.icons8.com/bubbles/2x/taco.png"
+            "img_path" => "taco-mexican-food-icon-fast-food-collection-food-icon-isolated_138676-531.jpg"
         ],
         [
             "name" => "Cinese",
             "color" => "red",
             "description" => "在中國菜中，“五種口味”之間的平衡以及因此的和諧非常重要。它們是鹹的，酸的，辛辣的，苦的和甜的。咸是最重要的，可以很好地增強每道菜的味道。所有的調味料都屬於這種風味。隨著課程的連續進行，食鹽的量逐漸減少。",
-            "img_path" => "https://www.svgrepo.com/show/26783/rice.svg"
+            "img_path" => "cibo-cinese.jpg"
         ],
         [
             "name" => "Italiano",
             "color" => "lightblue",
             "description" => "La cucina italiana è l'espressione dell'arte culinaria sviluppatasi in Italia, nonché l'insieme di tutte le sue gastronomie regionali, ed evolutasi attraverso secoli di cambiamenti politici e sociali, con radici che risalgono al IV secolo a.C. La cucina italiana stessa è stata influenzata dalla cucina dell'antica Grecia, dell'antica Roma, bizantina, ebraica e araba, ed è parte integrante dell'ampia famiglia delle cucine mediterranee.",
-            "img_path" => "https://cdn0.iconfinder.com/data/icons/cartoon-food/512/Food_512-25.png"
+            "img_path" => "italiano.jpg"
         ],
         [
             "name" => "Bevande",
             "color" => "blue",
             "description" => "Rinfrescanti bevande pronte per te",
-            "img_path" => "https://icons.iconarchive.com/icons/flat-icons.com/flat/512/Beer-icon.png"
+            "img_path" => "alcol-bevande.jpg"
         ]
     ]
 ?>

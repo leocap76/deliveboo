32824fee78ce039adf1e1d71140acce544b5a0ef
Alessio Pancia <alessiop2801@gmail.com>
2021-03-18 12:21:41 +0100

    style create dashboard

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -10,84 +10,84 @@
 
   <title>Creazione piatto</title>
 </head>
-<body>
+<body class="dashboard">
 
   @include('partials.header')
 
-  <div class="container">
+  <main>
+    <div class="container">
     
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-          @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-          @endforeach
-        </ul>
-      </div>
-    @endif
-
-    <form action="{{ route('admin.plates.store') }}" method="post" enctype="multipart/form-data">
-      @csrf
-      @method('POST')
+      @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
   
-      <div class="form-group">
-        <label for="name">Nome</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Nome del piatto" required maxlength="100">
-      </div>
+      <form action="{{ route('admin.plates.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        @method('POST')
+    
+        <div class="form-group">
+          <label for="name">Nome</label>
+          <input type="text" class="form-control" id="name" name="name" placeholder="Nome del piatto" required maxlength="100">
+        </div>
+    
+        <div class="form-group">
+          <label for="description">Descrizione</label>
+          <textarea name="description" id="description" rows="4" class="form-control" placeholder="Descrizione del piatto" required></textarea>
+        </div>
+    
+        <div class="form-group">
+          <label for="ingredients">Ingredienti</label>
+          <textarea name="ingredients" id="ingredients" rows="4" class="form-control" placeholder="Ingredienti del piatto" required></textarea>
+        </div>
+    
+        <div class="form-group">
+          <label for="price">Prezzo</label>
+          <input type="number" step="0.01"
+          class="form-control" id="price" name="price" placeholder="Prezzo del piatto" required min="0.01">
+        </div>
   
-      <div class="form-group">
-        <label for="description">Descrizione</label>
-        <textarea name="description" id="description" rows="4" class="form-control" placeholder="Descrizione del piatto" required></textarea>
-      </div>
+        <div class="form-group">
+          <label for="img_path">Immagine</label>
+          <input type="file" id="img_path" name="img_path" accept="image/*" required>
+        </div>
+    
+        <div class="form-group">
+          <label for="vegan">Vegano</label>
+          <input type="checkbox" id="vegan" name="vegan" value="1">
+        </div>
+    
+        <div class="form-group">
+          <label for="vegetarian">Vegetariano</label>
+          <input type="checkbox" id="vegetarian" name="vegetarian" value="1">
+        </div>
+    
+        <div class="form-group">
+          <label for="spicy">Piccante</label>
+          <input type="checkbox" id="spicy" name="spicy" value="1">
+        </div>
+    
+        <div class="form-group">
+          <label for="glutenfree">Gluten free</label>
+          <input type="checkbox" id="glutenfree" name="glutenfree" value="1">
+        </div>
+    
+        <div class="form-group">
+          <label for="available">Disponibile</label>
+          <input type="checkbox" id="available" name="available" value="1">
+        </div>
+    
   
-      <div class="form-group">
-        <label for="ingredients">Ingredienti</label>
-        <textarea name="ingredients" id="ingredients" rows="4" class="form-control" placeholder="Ingredienti del piatto" required></textarea>
-      </div>
+        <button type="submit" class="btn btn-success">Crea piatto</button>
   
-      <div class="form-group">
-        <label for="price">Prezzo</label>
-        <input type="number" step="0.01"
-        class="form-control" id="price" name="price" placeholder="Prezzo del piatto" required min="0.01">
-      </div>
-
-      <div class="form-group">
-        <label for="img_path">Immagine</label>
-        <input type="file" id="img_path" name="img_path" accept="image/*" required>
-      </div>
-  
-      <div class="form-group">
-        <label for="vegan">Vegano</label>
-        <input type="checkbox" id="vegan" name="vegan" value="1">
-      </div>
-  
-      <div class="form-group">
-        <label for="vegetarian">Vegetariano</label>
-        <input type="checkbox" id="vegetarian" name="vegetarian" value="1">
-      </div>
-  
-      <div class="form-group">
-        <label for="spicy">Piccante</label>
-        <input type="checkbox" id="spicy" name="spicy" value="1">
-      </div>
-  
-      <div class="form-group">
-        <label for="glutenfree">Gluten free</label>
-        <input type="checkbox" id="glutenfree" name="glutenfree" value="1">
-      </div>
-  
-      <div class="form-group">
-        <label for="available">Disponibile</label>
-        <input type="checkbox" id="available" name="available" value="1">
-      </div>
-  
-
-      <button type="submit" class="btn btn-success">Crea piatto</button>
-
-    </form>
-  </div>
-
- 
+      </form>
+    </div>
+  </main>
   
 </body>
 </html>
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,80 +4,88 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
   <script src="{{ asset('js/select2.js') }}" defer></script>
 
   <title>Crea un ristorante</title>
 </head>
-<body>
+<body class="dashboard">
 
-  <div class="container">
+  @include('partials.header')
 
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-          @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-          @endforeach
-        </ul>
-      </div>
-    @endif
+  <main>
+    <div class="container">
 
-    <form action="{{ route('admin.users.store') }}" method="post" enctype="multipart/form-data">
-      @csrf
-      @method('POST')
-  
-      <div class="form-group">
-        <label for="name">Nome del ristorante</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Nome del ristorante" value="{{ old('name') }}" required maxlength="100">
-      </div>
+      <h2>Crea il tuo ristorante</h2>
 
-      <div class="form-group">
-        <label for="address">Indirizzo del ristorante</label>
-        <input type="text" class="form-control" id="address" name="address" placeholder="Indirizzo del ristorante" value="{{ old('address') }}" required>
-      </div>
-  
-      <div class="form-group">
-        <label for="description">Descrizione del ristorante</label>
-        <textarea name="description" id="description" rows="4" class="form-control" placeholder="Descrizione del ristorante" required>{{ old('description') }}</textarea>
-      </div>
-
-      <div class="form-group">
-        <label for="img_path">Immagine del ristorante</label>
-        <input type="file" id="img_path" name="img_path" accept="image/*" required>
-      </div>
-
-      <div class="form-group">
-        <label for="PIVA">Partita IVA ristorante</label>
-        <input type="text" class="form-control" id="PIVA" name="PIVA" placeholder="Partita IVA" value="{{ old('PIVA') }}" required maxlength="11" minlength="11">
-      </div>
-
-      <div class="form-group">
-        <label for="opening_time">Orario apertura del ristorante</label>
-        <input type="time" class="form-control" id="opening_time" name="opening_time" placeholder="Orario di apertura (HH:MM:SS)" value="{{ old('opening_time') }}" required>
-      </div>
-
-      <div class="form-group">
-        <label for="closing_time">Orario chiusura del ristorante</label>
-        <input type="time" class="form-control" id="closing_time" name="closing_time" placeholder="Orario di chiusura (HH:MM:SS)" value="{{ old('closing_time') }}" required>
-      </div>
-
-
-      <div class="form-group">
-        <label for="category_id">Categorie</label>
-        <select class="form-control" name="category_id[]" id="category_id" multiple required>
-            @foreach ($categories as $category)          
-                <option value="{{ $category->id }}">{{ $category->name }}</option>
+      @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
             @endforeach
-        </select>
-      </div>
-
+          </ul>
+        </div>
+      @endif
   
-      <button type="submit" class="btn btn-success">Crea Ristorante</button>
+      <form action="{{ route('admin.users.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        @method('POST')
+    
+        <div class="form-group">
+          <label for="name">Nome del ristorante</label>
+          <input type="text" class="form-control" id="name" name="name" placeholder="Nome del ristorante" value="{{ old('name') }}" required maxlength="100">
+        </div>
   
-    </form>
-  </div>
+        <div class="form-group">
+          <label for="address">Indirizzo del ristorante</label>
+          <input type="text" class="form-control" id="address" name="address" placeholder="Indirizzo del ristorante" value="{{ old('address') }}" required>
+        </div>
+    
+        <div class="form-group">
+          <label for="description">Descrizione del ristorante</label>
+          <textarea name="description" id="description" rows="4" class="form-control" placeholder="Descrizione del ristorante" required>{{ old('description') }}</textarea>
+        </div>
+  
+        <div class="form-group">
+          <label for="img_path">Immagine del ristorante</label>
+          <input type="file" id="img_path" name="img_path" accept="image/*" required>
+        </div>
+  
+        <div class="form-group">
+          <label for="PIVA">Partita IVA ristorante</label>
+          <input type="text" class="form-control" id="PIVA" name="PIVA" placeholder="Partita IVA" value="{{ old('PIVA') }}" required maxlength="11" minlength="11">
+        </div>
+  
+        <div class="form-group">
+          <label for="opening_time">Orario apertura del ristorante</label>
+          <input type="time" class="form-control" id="opening_time" name="opening_time" placeholder="Orario di apertura (HH:MM:SS)" value="{{ old('opening_time') }}" required>
+        </div>
+  
+        <div class="form-group">
+          <label for="closing_time">Orario chiusura del ristorante</label>
+          <input type="time" class="form-control" id="closing_time" name="closing_time" placeholder="Orario di chiusura (HH:MM:SS)" value="{{ old('closing_time') }}" required>
+        </div>
+  
+  
+        <div class="form-group">
+          <label for="category_id">Categorie</label>
+          <select class="form-control" name="category_id[]" id="category_id" multiple required>
+              @foreach ($categories as $category)          
+                  <option value="{{ $category->id }}">{{ $category->name }}</option>
+              @endforeach
+          </select>
+        </div>
+  
+    
+        <button type="submit" class="btn btn-success">Crea Ristorante</button>
+    
+      </form>
+    </div>
+  </main>
  
-
 </body>
 </html>

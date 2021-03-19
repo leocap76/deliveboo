caa7df0cd59f1fd40aac395f20eec80758f19c5b
Leonardo Capogna <leonardocapogna@gmail.com>
2021-03-19 12:21:14 +0100

    shop restaurant page style

--- resources/views/shop/restaurant.blade.php
+++ resources/views/shop/restaurant.blade.php
@@ -14,7 +14,7 @@
     
     <title>Deliveboo - {{ $infoRestaurant->name }}</title> 
   </head>
-  <body>
+  <body class="restaurant_page">
     {{-- header --}}
     @include('partials.header')
     {{-- !header --}}
@@ -22,24 +22,140 @@
     <main>
       <section class="container" id="shop_restaurant">
         <div class="shop_restaurant_top">
-          <h1>{{ $infoRestaurant->name }}</h1>
-          <small>Indirizzo: {{ $infoRestaurant->address }}</small>
-          <p>Descrizione: {{ $infoRestaurant->description }}</p>
-          <img src="{{ ( str_contains($infoRestaurant->img_path, 'images/') ) ? asset('storage/' . $infoRestaurant->img_path) : $infoRestaurant->img_path }}" alt="{{ $infoRestaurant->name }}" style="width: 200px;">
+          <div class="left">
+            <h1>{{ $infoRestaurant->name }}</h1>
+            <small>indirizzo: {{ $infoRestaurant->address }}</small>
+            <h4>Descrizione:</h4>
+            <p> {{ $infoRestaurant->description }}</p>
+          </div>
+          <div class="right">
+            <img src="{{ ( str_contains($infoRestaurant->img_path, 'images/') ) ? asset('storage/' . $infoRestaurant->img_path) : $infoRestaurant->img_path }}" alt="{{ $infoRestaurant->name }}">
+          </div>
         </div>
 
-        <hr>
-
         <div class="shop_restaurant_bottom">
-          @foreach ($plates as $plate)
-            <div>
-              <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="">
-              <h3>{{ $plate->name }}</h3>
-              <p>Descrizione: {{ $plate->description }}</p>
-              <p>Ingredienti: {{ $plate->ingredients }}</p>
-              <h4>Prezzo: {{ $plate->price }}€</h4>
-            </div>
-          @endforeach
+          <div class="plates_container">
+            @foreach ($plates as $plate)
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div>
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div>
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div>
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+              <div class="shop_card_plates">
+                <img src="{{ asset( 'storage/' . $plate->img_path) }}" alt="{{ $plate->name }}">
+                <h3>{{ $plate->name }}</h3>
+                <p> {{ $plate->ingredients }}</p>
+                <h5> {{ $plate->price }}€</h5>
+                {{-- <p> {{ $plate->description }}</p> --}}
+              </div> 
+            @endforeach
+          </div>
+        {{-- carrello --}}
+          <div class="shop_cart">
+            <h3>Il tuo carrello</h3>
+            <h4>Prezzo totale:</h4>
+          </div>
+          {{-- carrello --}}
         </div>
         
       </section>

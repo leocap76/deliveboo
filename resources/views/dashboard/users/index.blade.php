fa14e8f54a1ce50c2ddce13a01795daf91ae19e5
Alex <alex.sircu@gmail.com>
2021-03-21 21:51:47 +0100

    dashboard side bar menu

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -18,6 +18,43 @@
 
         @include('partials.header')
 
+        <div id="dashboard_side_menu">
+            <ul>
+              <li></li>
+
+              <li>
+                <a href="{{ url('/') }}" class="buttons_style">Vai alla homepage</a>
+              </li>
+
+              <li>
+                <a href="{{ route('dashboard.users.edit', $user->id) }}" class="buttons_style">Modifica il tuo ristorante</a>
+              </li>
+
+              <li>
+                <a href="{{ route('dashboard.plates.index') }}" class="buttons_style">Visualizza i tuoi piatti</a>
+              </li>
+
+              <li>
+                <a href="" class="buttons_style">Visualizza i tuoi ordini</a>
+              </li>
+
+              <li>
+                @if (Route::currentRouteName() == 'dashboard.users.index' )
+                    @if (Route::has('login'))
+                        <div class="top-right links buttons_container">
+                            @auth
+                                <form action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="buttons_style">Logout</button>
+                                </form>
+                            @endauth    
+                        </div>
+                    @endif
+                @endif
+              </li>
+            </ul>            
+        </div>
+
         <main>
 
             <div class="container">

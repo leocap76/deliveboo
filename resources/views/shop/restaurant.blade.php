947262dce202cfb819459361a2db24aa3e865c9f
Leonardo Capogna <leonardocapogna@gmail.com>
2021-03-22 11:04:05 +0100

    favicon

--- resources/views/shop/restaurant.blade.php
+++ resources/views/shop/restaurant.blade.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.css" integrity="sha512-9iWaz7iMchMkQOKA8K4Qpz6bpQRbhedFJB+MSdmJ5Nf4qIN1+5wOVnzg5BQs/mYH3sKtzY+DOgxiwMz8ZtMCsw==" crossorigin="anonymous" />
     {{-- /fontawesome --}}
 
+    {{-- favicon --}}
+    <link rel="shortcut icon" type="image/x-icon"href="img/favicon.png">
+
     {{-- style --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">

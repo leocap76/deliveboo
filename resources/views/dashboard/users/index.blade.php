0de8e250d8819c2e9efc2179c497dd0767ba5c99
Alex <alex.sircu@gmail.com>
2021-03-24 01:27:04 +0100

    Dashboard mid container structure

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -18,78 +18,83 @@
 
         @include('partials.header')
 
+        <div class="dashboard_side_menu">
+          <ul>
+            <li>
+              <h3>{{ $user->name }}</h3>
+            </li>
+
+            <li>
+              <a href="{{ url('/') }}" class="buttons_style">Vai alla homepage</a>
+            </li>
+
+            <li>
+              <a href="{{ route('dashboard.users.edit', $user->id) }}" class="buttons_style">Modifica il tuo ristorante</a>
+            </li>
+
+            <li>
+              <a href="{{ route('dashboard.plates.index') }}" class="buttons_style">Visualizza i tuoi piatti</a>
+            </li>
+
+            <li>
+              <a href="" class="buttons_style">Visualizza i tuoi ordini</a>
+            </li>
+
+            <li>
+              @if (Route::currentRouteName() == 'dashboard.users.index' )
+                  @if (Route::has('login'))
+                      <div class="top-right links buttons_container">
+                          @auth
+                              <form action="{{ route('logout') }}" method="POST">
+                                  @csrf
+                                  <button type="submit" class="buttons_style">Logout</button>
+                              </form>
+                          @endauth    
+                      </div>
+                  @endif
+              @endif
+            </li>
+          </ul>            
+        </div>
+
         <main>
 
-          <div class="container-fluid">
+          {{-- <div id="dashboard_center"> --}}
 
-            <div id="dashboard_side_menu">
-              <ul>
-                <li>
-                  <img src="https://lh3.googleusercontent.com/proxy/bopozhBhe73VjFnoKuPGh9_XtQ5zdvYjSyCERjBxelsVtIV1uv2Y4Wvp811XID49h9zFjZqVM1BNwjPyv5hFNW5-BxXHOQhg39NB" alt="">
-                  <div>{{ $user->name }}</div>
-                </li>
-  
-                <li>
-                  <a href="{{ url('/') }}" class="buttons_style">Vai alla homepage</a>
-                </li>
-  
-                <li>
-                  <a href="{{ route('dashboard.users.edit', $user->id) }}" class="buttons_style">Modifica il tuo ristorante</a>
-                </li>
-  
-                <li>
-                  <a href="{{ route('dashboard.plates.index') }}" class="buttons_style">Visualizza i tuoi piatti</a>
-                </li>
-  
-                <li>
-                  <a href="" class="buttons_style">Visualizza i tuoi ordini</a>
-                </li>
-  
-                <li>
-                  @if (Route::currentRouteName() == 'dashboard.users.index' )
-                      @if (Route::has('login'))
-                          <div class="top-right links buttons_container">
-                              @auth
-                                  <form action="{{ route('logout') }}" method="POST">
-                                      @csrf
-                                      <button type="submit" class="buttons_style">Logout</button>
-                                  </form>
-                              @endauth    
-                          </div>
-                      @endif
-                  @endif
-                </li>
-              </ul>            
-            </div>
-  
-            <div id="dashboard_center">
+            {{-- <div class="monthly_stats">
+              <h2>Statistiche mensili</h2>
 
-              <div class="monthly_stats">
-                <h2>Statistiche mensili</h2>
-
-                <div class="monthly_stats_content">
-
-                </div>
-              </div>
-
-              <div class="last_orders">
-                <h2>Ultimi ordini</h2>
-
-                <div class="last_orders_content"> 
-
-                </div>
-              </div>
-
-              <div class="plates_in_menu">
-                <h2>Piatti presenti nel menu (per categoria)</h2>
-
-                <div class="plates_in_menu_content">
-
-                </div>
-              </div>
+              <div class="monthly_stats_content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam.</div>
             </div>
 
-          </div>
+            <div class="last_orders">
+              <h2>Ultimi ordini</h2>
+
+              <div class="last_orders_content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam.</div>
+            </div>
+
+            <div class="plates_in_menu">
+              <h2>Piatti presenti nel menu (per categoria)</h2>
+
+              <div class="plates_in_menu_content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam esse maiores assumenda provident quod. Culpa molestiae soluta provident, quasi distinctio eum aspernatur totam minima ullam rem eaque quisquam reiciendis quibusdam.</div>
+            </div> --}}
+  
+            <div id="dashboard_index_center">
+              <div class="dashboard_card info_restaurant">
+                <h2>{{ $user->infoRestaurant->name }}</h2>
+                <h3><i class="fas fa-map-marked-alt"></i> {{ $user->infoRestaurant->address }}</h3>
+                <h5><i class="fas fa-info-circle"></i> {{ $user->infoRestaurant->description }}</h5>
+                <p>Orario apertura: {{ $user->infoRestaurant->opening_time }}</p>
+                <p>Orario Chiusura: {{ $user->infoRestaurant->closing_time }}</p>
+                <p>Partita IVA: {{ $user->infoRestaurant->PIVA }}</p>
+              </div>
+              <div class="dashboard_card restaurant">
+                <img src="{{ ( str_contains($user->infoRestaurant->img_path, 'images/') ) ? asset('storage/' . $user->infoRestaurant->img_path) : $user->infoRestaurant->img_path }}" alt="{{ $user->infoRestaurant->name }}">
+              </div>
+              <div class="dashboard_card"></div>
+              <div class="dashboard_card"></div>
+            </div>
+            
 
         </main>
 
